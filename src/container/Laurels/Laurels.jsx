0e66ca013483d345b6a6ab6b8140d6a5d3bdd9c5
pxovelaa <gio.pxovelishvili@gmail.com>
2023-01-13 01:09:32 +0300

    photo gallery section ready

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -15,7 +15,7 @@ const AwardCard = ({award: {imgUrl, title, subtitle}}) => (
 );
 
 const Laurels = () => (
-  <div className='app__bg app__wrapper section__padding ' id='awards'>
+  <div className='app__bg app__wrapper section__padding ' id='award'>
     <div className="app__wrapper_info">
       <SubHeading title='Awards & recognation' />
       <h1 className='headtext__cormorant'>Our Laurels</h1>
